900e39685a24a152eb04589cb656bd55619574ed
Itsaadarsh <heyitsaadarsh@gmail.com>
2021-07-13 14:21:21 +0530

    Add models

---
+++ cmd/api/movie-handler.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"backend/models"
+	"errors"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+
+	if err != nil {
+		app.logger.Print(errors.New("invalid ID parameter"))
+	}
+
+	movie := models.Movie{
+		ID:          id,
+		Title:       "John Wick",
+		Description: "Action Movie",
+		Year:        2021,
+		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
+		Runtime:     120,
+		Rating:      5,
+		MPAARating:  "PG-16",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+}
+func (app *application) getAllMovie(w http.ResponseWriter, r *http.Request) {}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,8 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/", app.getAllMovie)
 
 	return router
 }
