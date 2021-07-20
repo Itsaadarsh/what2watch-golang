49d347df9a8c0ee22318f9400409b0d05b5a1d93
Itsaadarsh <heyitsaadarsh@gmail.com>
2021-07-20 12:38:03 +0530

    Adding auth middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 )
+
+func (app *application) wrap(next http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		ctx := context.WithValue(r.Context(), "params", ps)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+	secure := alice.New(app.checkToken)
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
@@ -19,9 +29,8 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.genreAll)
 
-	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editmovie)
-
-	router.HandlerFunc(http.MethodDelete, "/v1/admin/deletemovie/:id", app.deleteMovies)
+	router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editmovie)))
+	router.DELETE("/v1/admin/deletemovie/:id", app.wrap(secure.ThenFunc(app.deleteMovies)))
 
 	return app.enableCORS(router)
 }
